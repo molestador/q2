eaaff854733489b4ae2cec29225a87adab38b158
molestador <hustlerxxl@gmail.com>
2018-02-24 15:44:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let temp = [];
+  temp = equation.replace(/\s/g, '').replace(/\-/g, ' -').replace(/\+/g, ' +').trim().split(' ');
+  let a;
+  let b;
+  let c;
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i].search(/(\d+\*x\^2$|\-\d+\*x\^2$)/g) === 0) {
+      a = temp[i].replace(/\*x\^2/, '')
+    } else if (temp[i].search((/\+\d+\*x$|^\-\d+\*x$/)) === 0) {
+      b = temp[i].replace(/\*x/, '')
+    }
+    else {
+      c = temp[i];
+    }
+  }
+
+  let d = b * b - 4 * a * c;
+  let sol = [];
+    sol.push(Math.round((-b + Math.sqrt(d)) / (2 * a)));
+    sol.push(Math.round((-b - Math.sqrt(d)) / (2 * a)));
+
+    return sol.sort((a, b) => a - b);
 }
